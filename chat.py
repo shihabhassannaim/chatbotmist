cd05f6b247a01c1215159c60a87f7ff038a3edfc
Shihab Hassan Naim <hassanshihab231@gmail.com>
2025-03-07 10:27:24 +0600

    rq

--- chat.py
+++ chat.py
@@ -42,7 +42,7 @@ app = FastAPI()
 # Allow all origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*" , "http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
