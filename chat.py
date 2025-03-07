da8885ae711c04b1636c414406091d60a2efbce0
Shihab Hassan Naim <hassanshihab231@gmail.com>
2025-03-07 10:08:43 +0600

    rq

--- chat.py
+++ chat.py
@@ -35,6 +35,20 @@ def get_best_answer(query, index, questions, answers):
 # Initialize FastAPI
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 data = load_university_data()
 index, questions, answers = create_vector_store(data)
 
